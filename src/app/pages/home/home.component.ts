c2b3cd231ddcae55505e0c625a4b6b4ce90824e5
Rafael Bernal Gallegos <rbernalgallegos@gmail.com>
2025-04-23 13:50:37 -0600

    Responsividad terminada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AboutMeComponent } from "../../components/about-me/about-me.component";
 import { ToolsCardComponent } from "../../components/tools-card/tools-card.component";
 import { WorkComponent } from "../../components/work/work.component";
+import { ContactMeComponent } from "../../components/contact-me/contact-me.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutMeComponent, ToolsCardComponent, WorkComponent],
+  imports: [AboutMeComponent, ToolsCardComponent, WorkComponent, ContactMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
