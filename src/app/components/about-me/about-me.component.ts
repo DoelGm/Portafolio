673b1c43b549b994faf8447546cd5e6dc3571b08
Rafael Bernal Gallegos <rbernalgallegos@gmail.com>
2025-05-25 16:04:05 -0600

    modificacion de el welcome

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { ToolsCardComponent } from "../tools-card/tools-card.component";
+
 
 @Component({
   selector: 'app-about-me',
-  imports: [ToolsCardComponent],
+  imports: [],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
